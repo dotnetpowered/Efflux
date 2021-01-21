dd79452e293f13b614b99526b80b53aa0926200e
Brian Ritchie <brian.ritchie@gmail.com>
2021-01-21 06:35:44 -0500

    Add Messaging sample

---
+++ samples/Efflux.Samples.Messaging/Messaging.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Efflux.Samples.Messaging
+{
+    public static class MessagingSample
+    {
+        public static async Task Demo(ITopic topic)
+        {
+            // Message Stream access method (write to end, random read)
+            // ------------------------------------------------------------
+            var start = DateTime.Now;
+            long readOffset = 0;
+            for (var i = 0; i < 100; i++)
+            {
+                var message = new EffluxMessage("hello world Booen39gb9j23binakd " + i);
+                var offset = await topic.WriteMessageAsync(message);
+
+                Console.WriteLine($"Wrote {i}: {message.MetaData.Id} at {offset}");
+
+                if (i == 50)
+                {
+                    readOffset = offset;
+                }
+            }
+            Console.WriteLine($"Write Duration: {(DateTime.Now - start).TotalMilliseconds / 1000.0}s");
+
+            var readResult = await topic.ReadMessageAsync(readOffset);
+            Console.WriteLine($"Read from {readOffset} found id={readResult.Message.MetaData.Id} data='{readResult.Message.PayloadAsString()}'");
+
+            // Consumer access method (resumable forward-only reader)
+            // ------------------------------------------------------------
+            Console.WriteLine("Starting consumer:");
+            MessageReadResult result = new MessageReadResult();
+            var consumer = await topic.CreateConsumerAsync("consumer1");
+            Console.WriteLine($"Starting consumer at {consumer.CurrentOffset}");
+
+            while (!result.EndOfStream)
+            {
+                result = await consumer.ReadMessageAsync(true);
+                if (result.Message != null)
+                    Console.WriteLine(result.Message.PayloadAsString());
+            }
+        }
+    }
+}
---
+++ samples/Efflux.Samples.Messaging/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System.Threading.Tasks;
+using Efflux.Stream;
+
+namespace Efflux.Samples.Messaging
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.AddConsole(options => {
+                        options.FormatterName = ConsoleFormatterNames.Systemd;
+                    });
+                })
+                .AddTransient<ITopicFactory, TopicStreamFactory>()
+                .BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+            logger.LogDebug("Starting application");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            var factory = serviceProvider.GetService<ITopicFactory>();
+            var topic = factory.OpenTopic("my-topic");
+            await MessagingSample.Demo(topic);
+        }
+    }
+}
