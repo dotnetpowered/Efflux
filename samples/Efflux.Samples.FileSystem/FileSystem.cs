f97dfad2611a5d59c2b16332c07ba549c14c12ea
Brian Ritchie <brian.ritchie@gmail.com>
2021-01-21 06:19:31 -0500

    Add FileSystem sample

---
+++ samples/Efflux.Samples.FileSystem/FileSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Efflux.FileSystem;
+
+namespace Efflux.Samples.FileSystem
+{
+    public static class FileSystemSample
+    {
+        // Immutable File System
+        public static void Demo(ITopic topic)
+        {
+            IFileSystem fileSystem = new ImmutableFileSystem(topic);
+
+            var path = new FileSystemPath().AppendDirectory("opt").AppendDirectory(Guid.NewGuid().ToString()).AppendFile("temp.dat");
+            using (var stream = fileSystem.CreateFile(path))
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.WriteLine("Hello World");
+                writer.WriteLine("This rocks");
+            }
+
+            Console.WriteLine($">> Read file from path {path}");
+            using (var stream = fileSystem.OpenFile(path, FileAccess.Read))
+            using (var reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                }
+            }
+        }
+    }
+}
---
+++ samples/Efflux.Samples.FileSystem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Efflux.Stream;
+
+namespace Efflux.Samples.FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.AddConsole(options => {
+                        options.Format = ConsoleLoggerFormat.Systemd;
+                    });
+                })
+                .AddTransient<ITopicFactory, TopicStreamFactory>()
+                .BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+            logger.LogDebug("Starting application");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Efflux FileSystem Sample");
+
+            var factory = serviceProvider.GetService<ITopicFactory>();
+            var topic = factory.OpenTopic("fs-topic");
+
+            FileSystemSample.Demo(topic);
+        }
+    }
+}
